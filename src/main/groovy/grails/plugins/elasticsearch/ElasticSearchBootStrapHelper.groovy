40717d9c8728d9cff9d72fa80623c24ec5782098
Eric Chauvin <echauvin@webbfontaine.com>
2017-06-01 09:17:49 +0200

    fix static type checking

--- src/main/groovy/grails/plugins/elasticsearch/ElasticSearchBootStrapHelper.groovy
+++ src/main/groovy/grails/plugins/elasticsearch/ElasticSearchBootStrapHelper.groovy
@@ -34,7 +34,7 @@ class ElasticSearchBootStrapHelper implements ElasticSearchConfigAware {
             elasticSearchService.index(domainsToReindex)
         } else if (bulkIndexOnStartup) { //Index all
             LOG.debug "Performing bulk indexing."
-            elasticSearchService.index()
+            elasticSearchService.index(null as Class[])
         }
         //Update index aliases where needed
         MappingMigrationStrategy migrationStrategy = migrationConfig?.strategy ? MappingMigrationStrategy.valueOf(migrationConfig?.strategy as String) : none
